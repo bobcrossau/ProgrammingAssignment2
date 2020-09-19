3f4a6ac7dcbd0d7896cce36fe3c0939a882884bd
bobcrossau <bobcross@outlook.com.au>
2020-09-19 15:43:31 +0800

    Updated with comments of my understanding of the operation

--- cachesolve.R
+++ cachesolve.R
@@ -1,11 +1,24 @@
-cachesolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
+cachesolve <- function(func, ...) {
+  # The purpose of this function is to return a previously  
+  # cached inverse matrix. The function takes as its input  
+  # the 'makeCacheMatrix' function, which has cached in the 
+  # parent environment an object with functions for use by 
+  # this function.
+  # First it accesses the inverse matrix in memory
+  invmat <- func$getinverse()
+  # If it isn't NULL - i.e. it's a valid result
+  # it returns the inverse matrix and the function ends.
+  if(!is.null(invmat)) {
     message("getting cached data")
-    return(m)
+    return(invmat)
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+  # If a correct inverse matrix is unavailable it gets 
+  # the input matrix ...
+  data <- func$get()
+  # and solves it ...
+  invmat <- solve(data, ...)
+  # and returns the solution to the parent environment
+  func$setinverse(m)
+  # and prints the inverse matrix
+  invmat
 }
