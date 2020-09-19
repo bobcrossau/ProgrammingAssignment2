b3d707c8ec11c5cc58e6a5ed1b8a9bd054ded043
bobcrossau <bobcross@outlook.com.au>
2020-09-19 15:41:59 +0800

    Updated with comments of understanding of operation

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,25 @@
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+makeCacheMatrix <- function(input = matrix()) {
+  inverse <- NULL # define a variable
+  # the function is input as a variable by the 'cacheSolve' 
+  # function to so that the time-consuming calculation of
+  # an inverse matrix can be used repeatedly. The function
+  # holds four functions - set, get, setinverse & getinverse.
+  # Code defines 1st function: set() enables later input
+  set <- function(matx) { 
+    input <<- matx  # assigns input in the parent environment
+    inverse <<- NULL # assigns NULL value in the parent environment
   }
-  get <- function()x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function()m
+  # Code defines 2nd function: the getter for the input matrix
+  get <- function()input
+  # Code defines 3rd function: the setter for the inverse matrix
+  setinverse <- function(solve) inverse <<- solve
+  # Code defines 4th function: the getter for the inverse matrix
+  getinverse <- function()inverse
+  # create a 'special' object that is each of the four functions 
+  # in a list. The object is held in memory in the parent environment
+  # rather than being cleaned up by the garbage collector. The 
+  # functions in the list can be later accessed in the cacheSolve
+  # function by name using $ rather than a less elegant [[ form.
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
